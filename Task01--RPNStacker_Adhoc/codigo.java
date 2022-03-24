32626ee17039124c8656f832efcb85f91225df3d
malu lima <mlll@cin.ufpe.br>
2022-03-24 11:58:38 -0300

    initial commit

---
+++ Task01--RPNStacker_Adhoc/codigo.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.Stack;
+import java.io.FileNotFoundException;
+
+public class codigo {
+    public static void main(String[] args) throws FileNotFoundException {
+        Stack<String> stack = new Stack<String>();
+        File file = new File("Calc1.txt");
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()) {
+            String c = sc.nextLine();
+            System.out.println(stack);
+
+            if(c.equals("/") || c.equals("+") || c.equals("-") || c.equals("*")){
+                if(c.equals("+")){
+                    String a = stack.pop();
+                    String b = stack.pop();
+                    double d = Double.parseDouble(a) + Double.parseDouble(b);
+                    String e = String.valueOf(d);
+                    stack.push(e);
+                }
+                else if(c.equals("-")){
+                    String a = stack.pop();
+                    String b = stack.pop();
+                    double d = Double.parseDouble(b) - Double.parseDouble(a);
+                    String e = String.valueOf(d);
+                    stack.push(e);
+                }
+                else if(c.equals("*")){
+                    String a = stack.pop();
+                    String b = stack.pop();
+                    double d = Double.parseDouble(a) * Double.parseDouble(b);
+                    String e = String.valueOf(d);
+                    stack.push(e);
+                }
+                else if(c.equals("/")){
+                    String a = stack.pop();
+                    String b = stack.pop();
+                    double d = Double.parseDouble(b) / Double.parseDouble(a);
+                    if(Math.abs(d)<Double.POSITIVE_INFINITY) {
+                        String e = String.valueOf(d);
+                        stack.push(e);
+                    }
+                    else{
+                        System.out.println("Divisao por zero");  
+                    }
+                }
+            }
+            else 
+                stack.push(c);
+        }
+        System.out.println(stack);
+        String f = stack.pop();
+        System.out.println("O resultado eh: " + f);
+        sc.close();
+    }
+}
